f938d036ae9ecf8275137056489663124ffd7e71
vinayakgarole <vinayakgarole10@gmail.com>
2022-08-30 16:56:26 +0530

    added: day 3 assignment

---
+++ src/PrintArray.java
@@ -0,0 +1,9 @@
+public class PrintArray {
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50};
+        int i;
+        for (i=0; i<=arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
